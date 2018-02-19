df1f0237cd6e0642ea1efcbf51b4fa1ee257c747
Intae Na <itna@vadas.co.kr>
2018-02-19 18:04:58 +0900

    Hello world init

---
+++ hello.cpp
@@ -0,0 +1,17 @@
+/*!
+ *	@file	hello.cpp
+ *	@author	itna
+ *	@email 	itna@vadas.co.kr
+ *	@brief	Hello, world!!
+ */
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	/// Print out string
+	cout << "Hello, world!!" << endl;
+	
+	/// Always return
+	return 0;	
+}
