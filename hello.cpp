325d0c0bf78c2edeca5bc815a6b61dc6a6b1e3a3
Intae Na <itna@vadas.co.kr>
2018-02-19 18:27:17 +0900

    TROLL : oops, did it again

--- hello.cpp
+++ hello.cpp
@@ -11,6 +11,9 @@ using namespace std;
 int main(int argc, char *argv[]) {
 	/// Print out string
 	cout << "Hello, world!!" << endl;
+
+	/// Troll comes in, and messed up build again
+	int b = a + 1;
 	
 	/// Always return
 	return 0;	
